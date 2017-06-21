6b935a28a0dfae875000a9b363a2425537af34c4
Olavi Haapala <ojhaapala@gmail.com>
2017-06-21 23:41:45 +0300

    Add tests for price formatter

--- src/utils/utils.price.test.ts
+++ src/utils/utils.price.test.ts
@@ -1,7 +1,7 @@
 import xs from 'xstream';
 import { forall, integer, assert, Options } from 'jsverify';
 
-import calculatePrice from './price';
+import calculatePrice, { formatPrice } from './price';
 
 const testOptions: Options = {
   tests: 100,
@@ -9,34 +9,62 @@ const testOptions: Options = {
   size: 60 * 60 * 24
 };
 
-describe('calculatePrice tests', () => {
-  it('should work for all numbers', () => {
-    const property = forall(
-      integer,
-      integer,
-      integer,
-      (x, y, z) =>
-        calculatePrice(x, y, z) + calculatePrice(x, y, z) ===
-        2 * calculatePrice(x, y, z)
-    );
+describe('Price utils', () => {
+  describe('calculatePrice', () => {
+    it('should work for all numbers', () => {
+      const property = forall(
+        integer,
+        integer,
+        integer,
+        (x, y, z) =>
+          calculatePrice(x, y, z) + calculatePrice(x, y, z) ===
+          2 * calculatePrice(x, y, z)
+      );
 
-    assert(property, testOptions);
+      assert(property, testOptions);
+    });
+
+    it('should return correct value', () => {
+      const personAmount = 12;
+      const avgPrice = 88;
+      const tick = 60 * 60 * 2;
+      const expected = 2112;
+      const actual = calculatePrice(personAmount, avgPrice, tick);
+      expect(actual).toBe(expected);
+    });
+
+    it('should return 0 if tick is zero', () => {
+      const personAmount = 4;
+      const avgPrice = 100;
+      const tick = 0;
+      const actual = calculatePrice(personAmount, avgPrice, tick);
+      expect(actual).toBe(0);
+    });
   });
 
-  it('should return correct value', () => {
-    const personAmount = 12;
-    const avgPrice = 88;
-    const tick = 60 * 60 * 2;
-    const expected = 2112;
-    const actual = calculatePrice(personAmount, avgPrice, tick);
-    expect(actual).toBe(expected);
-  });
+  describe('formatPrice', () => {
+    it('should return correctly formatted string for €', () => {
+      const price = 1234.56789;
+      const currency = '€';
+      const expected = '1234,57 €';
+      const actual = formatPrice(price, currency);
+      expect(actual).toBe(expected);
+    });
 
-  it('should return 0 if tick is zero', () => {
-    const personAmount = 4;
-    const avgPrice = 100;
-    const tick = 0;
-    const actual = calculatePrice(personAmount, avgPrice, tick);
-    expect(actual).toBe(0);
+    it('should return correctly formatted string for $', () => {
+      const price = 1234.56789;
+      const currency = '$';
+      const expected = '$ 1234.57';
+      const actual = formatPrice(price, currency);
+      expect(actual).toBe(expected);
+    });
+
+    it('should format an integer correctly', () => {
+      const price = 15656;
+      const currency = '€';
+      const expected = '15656,00 €';
+      const actual = formatPrice(price, currency);
+      expect(actual).toBe(expected);
+    });
   });
 });
